7d32919218c457c5d40af78e08990c5f1202a07e
Fernando dos Santos <nando4803@gmail.com>
2020-08-27 13:13:38 +0100

    This commit fix api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import api from './server/api';
 
 function App() {
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await api.getHomeList();
+      console.log(list);
+    }
+    loadAll();
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
--- src/server/api.js
+++ src/server/api.js
@@ -13,48 +13,48 @@ export default {
       {
         slug: 'originais',
         title: 'Originais Netflix',
-        items: await basicFetch(`/discover/tv?with_network=213&language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/discover/tv?api_key=${API_KEY}`)
       },
 
       {
         slug: 'tranding',
         title: 'Recomendados para Você',
-        items: await basicFetch(`/tranding/all/week?language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/trending/all/week?api_key=${API_KEY}`)
       },
 
       {
         slug: 'toprated',
         title: 'Em Alta',
-        items: await basicFetch(`/move/top_rated?language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/movie/top_rated?api_key=${API_KEY}`)
       },
 
       {
         slug: 'action',
         title: 'Ação',
-        items: await basicFetch(`/discover/move?with_genres=28&language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/discover/movie?with_genres=28&api_key=${API_KEY}`)
       },
 
       {
         slug: 'comedy',
         title: 'Comédia',
-        items: await basicFetch(`/discover/move?with_genres=35&language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/discover/movie?with_genres=35&api_key=${API_KEY}`)
       },
 
       {
         slug: 'horror',
         title: 'Terror',
-        items: await basicFetch(`/discover/move?with_genres=27&language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/discover/movie?with_genres=27&api_key=${API_KEY}`)
       },
 
       {
         slug: 'romance',
         title: 'Romance',
-        items: await basicFetch(`/discover/move?with_genres=10749&language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/discover/movie?with_genres=10749&api_key=${API_KEY}`)
       },
       {
         slug: 'documentary',
         title: 'Documentários',
-        items: await basicFetch(`/discover/move?with_genres=99&language=pt-BR&api_key${API_KEY}`)
+        items: await basicFetch(`/discover/movie?with_genres=99&api_key=${API_KEY}`)
       }
     ]
   }
