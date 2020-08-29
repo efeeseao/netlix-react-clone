ad7d72ac6553691197fe4ca0de85d4be19c31e8c
Fernando dos Santos <nando4803@gmail.com>
2020-08-29 23:10:40 +0100

    This commit update movieRow

--- src/components/MovieListRow/index.js
+++ src/components/MovieListRow/index.js
@@ -27,20 +27,20 @@ export default ({title, items}) =>{
     return(
         <div className="movieRow">
             <h2>{title}</h2>
-            <div className="movieRow-left" onClick={handleLeftArrow}>
-                <NavigateBeforeIcon style={{fontSize:50}} />
+            <div className="movieRowLeft" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon  className="navigation" />
             </div>
-            <div className="movieRow-right" onClick={handleRightArrow}>
-                <NavigateNextIcon style={{fontSize:50}} />
+            <div className="movieRowRight" onClick={handleRightArrow}>
+                <NavigateNextIcon className="navigation" />
             </div>
 
-            <div className="movieRow--listarea">
-                <div className="movieRow--list"style={{
+            <div className="movieRowListarea">
+                <div className="movieRowList"style={{
                     marginLeft :scrollX,
                     width: items.results.length * 150
                 }}>
                     {items.results.length > 0 && items.results.map((item, key)=>(
-                        <div key={key} className="movieRow--item">    
+                        <div key={key} className="movieRowItem">    
                             <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
                         </div>
                     ))}
