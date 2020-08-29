4495cb0ad5d8a2fa79442c01bc0360a686c78159
Fernando dos Santos <nando4803@gmail.com>
2020-08-30 00:05:51 +0100

    This commit update header Icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,31 +1,32 @@
 import React from 'react';
-import { KeyboardArrowDown, NotificationImportant, PresentToAll, Search } from '@material-ui/icons';
+import { AiFillGift, AiOutlineSearch } from 'react-icons/ai';
+import { IoIosNotifications, IoIosArrowDown } from 'react-icons/io';
 
 import './styles.css';
 
 export default ({black})=>{
-    return(
-        <header className={black ?"black" : ''}>
-            <div className="logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"/>
-            </div>
-            <nav>
-                <ul>
-                    <li>Inicio</li>
-                    <li>Séries</li>
-                    <li>Filmes</li>
-                    <li>Mais recentes</li>
-                    <li>A minha lista</li>
-                </ul>
-            </nav>
-            <div className="userIcon">
-                <Search color="#FFF" className="search" />
-                <h4 className="child">Criança</h4>
-                <PresentToAll color="#FFF" className="gift" />
-                <NotificationImportant color="#FFF" className="notification" />
-                <img src="https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png"/>
-                <KeyboardArrowDown color="#FFF" />
-            </div>
-        </header>
-    )
+    return (
+      <header className={black ? 'black' : ''}>
+        <div className='logo'>
+          <img src='https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png' />
+        </div>
+        <nav>
+          <ul>
+            <li>Inicio</li>
+            <li>Séries</li>
+            <li>Filmes</li>
+            <li>Mais recentes</li>
+            <li>A minha lista</li>
+          </ul>
+        </nav>
+        <div className='userIcon'>
+          <AiOutlineSearch className='search' />
+          <h4 className='child'>Criança</h4>
+          <AiFillGift className='gift' />
+          <IoIosNotifications className='notification' />
+          <img src='https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png' />
+          <IoIosArrowDown />
+        </div>
+      </header>
+    );
 }
