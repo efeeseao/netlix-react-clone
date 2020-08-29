a73e4a016988eb6d7a800f7bd019445690fff188
Fernando dos Santos <nando4803@gmail.com>
2020-08-29 15:20:19 +0100

    This commit update all site

--- src/server/api.js
+++ src/server/api.js
@@ -1,61 +1,72 @@
 const API_KEY = '51fd99d5677ddd16b1fe835873e08731';
 const API_BASE = 'https://api.themoviedb.org/3';
 
-const basicFetch = async (endpoint) => {
-  const req = await fetch(`${API_BASE}${endpoint}`);
-  const json = await req.json();
-  return json;
+
+const basicFecth = async (endpoint) => {
+  return (await fetch(`${API_BASE}${endpoint}`)).json();
 }
 
 export default {
   getHomeList: async () => {
-    return [
-      {
-        slug: 'originais',
-        title: 'Originais Netflix',
-        items: await basicFetch(`/discover/tv?api_key=${API_KEY}`)
+    return [{
+        slug: 'originals',
+        title: "Originais do Netflix",
+        items: await basicFecth(`/discover/tv/?with_network=213&language=pt-BR&api_key=${API_KEY}`)
       },
-
       {
-        slug: 'tranding',
-        title: 'Recomendados para Você',
-        items: await basicFetch(`/trending/all/week?api_key=${API_KEY}`)
+        slug: 'trending',
+        title: "Recomendados para Você",
+        items: await basicFecth(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
       },
-
       {
         slug: 'toprated',
-        title: 'Em Alta',
-        items: await basicFetch(`/movie/top_rated?api_key=${API_KEY}`)
+        title: "Em Alta",
+        items: await basicFecth(`/movie/top_rated?&language=pt-BR&api_key=${API_KEY}`)
       },
-
       {
         slug: 'action',
-        title: 'Ação',
-        items: await basicFetch(`/discover/movie?with_genres=28&api_key=${API_KEY}`)
+        title: "Ação",
+        items: await basicFecth(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
       },
-
       {
         slug: 'comedy',
-        title: 'Comédia',
-        items: await basicFetch(`/discover/movie?with_genres=35&api_key=${API_KEY}`)
+        title: "Comédia",
+        items: await basicFecth(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
       },
-
       {
         slug: 'horror',
-        title: 'Terror',
-        items: await basicFetch(`/discover/movie?with_genres=27&api_key=${API_KEY}`)
+        title: "Terror",
+        items: await basicFecth(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
       },
-
       {
         slug: 'romance',
-        title: 'Romance',
-        items: await basicFetch(`/discover/movie?with_genres=10749&api_key=${API_KEY}`)
+        title: "Romance",
+        items: await basicFecth(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
       },
       {
         slug: 'documentary',
-        title: 'Documentários',
-        items: await basicFetch(`/discover/movie?with_genres=99&api_key=${API_KEY}`)
-      }
+        title: "Documentários",
+        items: await basicFecth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+      },
     ]
+  },
+
+  getMovieInfo: async (movieId, type) => {
+    let info = {};
+    if (movieId) {
+      switch (type) {
+        case 'movei':
+          info = await basicFecth(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+          break;
+        case 'tv':
+          info = await basicFecth(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+          break;
+        default:
+          info = null;
+          break;
+      }
+    }
+
+    return info;
   }
 }
