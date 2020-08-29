8480853f80ed03622d18408d81e732f619e2b355
Fernando dos Santos <nando4803@gmail.com>
2020-08-29 23:05:25 +0100

    This commit update featuredMovie

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -2,9 +2,6 @@ import React from 'react';
 import './styles.css';
 
 export default ({item}) => {
-
-    let firstDate = new Date(item.first_air_date);
-
     let genres = [];
     for(let i in item.genres){
         genres.push(item.genres[i].name);
@@ -22,20 +19,19 @@ export default ({item}) => {
             backgroundPosition: 'center',
             backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
         }}>
-            <div className="featured--vertical">
-                <div className="featured--horizontal">
-                    <div className="featured--name">{item.original_name}</div>
-                    <div className="featured--info">
-                        <div className="featured--points">{item.vote_average} pontos</div>
-                        <div className="featured--year">{firstDate.getFullYear()}</div>
-                        <div className="featured--seasons">{item.number_of_seasons} temporada{item.number_of_season !== 1 ? 's' : ''}</div>
+            <div className="featuredVertical">
+                <div className="featuredHorizontal">
+                    <div className="featuredName">{item.original_name}</div>
+                    <div className="featuredInfo">
+                        <div className="featuredSeasons">
+                        {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
                     </div>
-                        <div className="featured--description">{descr}</div>
-                        <div className="featured--buttons">
-                            <a href={`/watch/${item.id}`} className="featured--watchbutton">► Assistir</a>
-                            <a href={`/list/add/${item.id}`} className="featured--mylistbutton">+ Minha Lista</a>
+                        <div className="featuredDescription">{descr}</div>
+                        <div className="featuredButtons">
+                            <a href={`/watch/${item.id}`} className="featuredWatchbutton">► Ver</a>
+                            <a href={`/list/add/${item.id}`} className="featuredMoreInfo">i Mais informações</a>
                         </div>
-                        <div className="featured--genres">Gêneros: <strong> {genres.join(', ')} </strong></div>
+                        <div className="featuredGenres">Gêneros: <strong> {genres.join(', ')} </strong></div>
 
                 </div>
             </div>
