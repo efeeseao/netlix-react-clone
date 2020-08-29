e0aceead7541e8afef71172d66dd42f4603b7b4d
Fernando dos Santos <nando4803@gmail.com>
2020-08-29 23:30:30 +0100

    This commit update movieRow

--- src/components/MovieListRow/index.js
+++ src/components/MovieListRow/index.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 import './styles.css';
-import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
-import NavigateNextIcon from '@material-ui/icons/NavigateNext';
+import { NavigateBefore } from 'styled-icons/material';
+import { NavigateNext } from 'styled-icons/material';
 
 export default ({title, items}) =>{
 
     const [scrollX, setScrollX]= useState(0);
 
     const handleLeftArrow = () => {
-        let x = scrollX + Math.round(window.innerWidth / 2);
+        let x = scrolNavigateNextIconlX + Math.round(window.innerWidth / 2);
         if(x > 0){
             x = 0;
         }
@@ -24,28 +24,34 @@ export default ({title, items}) =>{
         setScrollX(x);
     }
 
-    return(
-        <div className="movieRow">
-            <h2>{title}</h2>
-            <div className="movieRowLeft" onClick={handleLeftArrow}>
-                <NavigateBeforeIcon  className="navigation" />
-            </div>
-            <div className="movieRowRight" onClick={handleRightArrow}>
-                <NavigateNextIcon className="navigation" />
-            </div>
-
-            <div className="movieRowListarea">
-                <div className="movieRowList"style={{
-                    marginLeft :scrollX,
-                    width: items.results.length * 150
-                }}>
-                    {items.results.length > 0 && items.results.map((item, key)=>(
-                        <div key={key} className="movieRowItem">    
-                            <img  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.original_title} />
-                        </div>
-                    ))}
-                </div>
-            </div>
+    return (
+      <div className='movieRow'>
+        <h2>{title}</h2>
+        <div className='movieRowLeft' onClick={handleLeftArrow}>
+          <NavigateBefore className='navigation' />
         </div>
+        <div className='movieRowRight' onClick={handleRightArrow}>
+          <NavigateNext className='navigation' />
+        </div>
+
+        <div className='movieRowListarea'>
+          <div
+            className='movieRowList'
+            style={{
+              marginLeft: scrollX,
+              width: items.results.length * 150,
+            }}>
+            {items.results.length > 0 &&
+              items.results.map((item, key) => (
+                <div key={key} className='movieRowItem'>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                    alt={item.original_title}
+                  />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
     );
 }
