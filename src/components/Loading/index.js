6fd83bd23978374f580cceb758a231e99aaa692b
Fernando dos Santos <nando4803@gmail.com>
2020-08-29 16:10:25 +0100

    This commit add loading

---
+++ src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+function Loading() {
+  return (
+    <div className="loading">
+        <img src="https://cdn.lowgif.com/small/0534e2a412eeb281-the-counterintuitive-tech-behind-netflix-s-worldwide.gif" alt="Loading"></img>
+      </div>
+  )
+}
+
+export default Loading;
